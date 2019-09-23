0b244192c1d890bd5949b463f0f401d1dbf1c383
DInnaD <DInnaD@users.noreply.github.com>
2019-09-23 20:52:03 +0300

    Update OrganizationCollection.php

--- app/Http/Resources/OrganizationCollection.php
+++ app/Http/Resources/OrganizationCollection.php
@@ -14,15 +14,13 @@ class OrganizationCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-       public $collects = 'App\Http\Resources\OrganizationResource';
         /**
          * Transform the resource collection into an array.
          *
          * @param  \Illuminate\Http\Request  $request
          * @return array
          */
-        public function toArray($request)
-        {
+       
             return [
                 'success' => true,
                 'data' => $this->collection,
