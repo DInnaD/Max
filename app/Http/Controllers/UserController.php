b59c3fdfb71c5d5dab78f0af1615d51211bc0c55
DInnaD <DInnaD@users.noreply.github.com>
2019-09-27 11:09:20 +0300

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,6 +16,7 @@ class UserController extends Controller
 {   
     public function index(Request $request)
     {
+        //$this->authorize(User::class);
         return new UserCollection(User::getSearchList($request));
         
     }
@@ -28,42 +29,32 @@ class UserController extends Controller
     public function indexStats(Request $request)
     {
        $this->authorize(User::class);
-       $users = \App\Http\Resources\UserCollection::make(User::all());
-       $worker = $users->filter(function ($value){
-            return $value->role == 'worker';
-       })->count();
-       $employer = $users->filter(function ($value){
-            return $value->role == 'employer';
-       })->count();
-       $admin = $users->filter(function ($value){
-            return $value->role == 'admin';
-       })->count();
-        $user = collect(['worker' =>  $worker, 'employer' => $employer, 'admin' => $admin]);
-        return response()->json(['success' => true, 'data' => $user], 200);
-                
+       $user = User::getRoleList($request);
         
+       return response()->json(['success' => true, 'data' => $user], 200);   
     }
 
     public function show(User $user, Vacancy $vacancy)
     {
-       $this->authorize(User::class);
-        return response()->json(['success' => true, 'data' => $user->load('vacancies')
-        ], 200);
+       //$this->authorize('show', $user);
+        
+       return new UserResource($user);
     }
 
     public function update(UserRequest $request, User $user)
     {
-       $this->authorize(User::class);//alone
-
+        //$this->authorize('update', $user);
         $user->update($request->all());
-        return response()->json($user);
+        
+        return new UserResource($user);
 
     }
 
     public function destroy(UserRequest $request, User $user)
     {
+        //$this->authorize('delete', $user);
+        $user->delete();
 
-       $this->authorize(User::class);
-        return response()->json(['success' => $user->delete()], 200);
+        return new UserResource($user);
     }
 }
